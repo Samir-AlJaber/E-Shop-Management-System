22152186d83a56e70dbd9b18720f562e0ed67c5b
unknown <samir.cse.20230104136@aust.edu>
2026-02-28 16:53:09 +0600

    Footer component added

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2026 E-Shop Management System. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
